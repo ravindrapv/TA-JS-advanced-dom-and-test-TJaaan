7117a37725415a2b02d86a547a82e053a5bd9582
ravindrapv <ravindra.ravi4849@gmail.com>
2022-05-12 16:24:32 +0530

    test-assignment

--- block-BJaajc/code/index.test.js
+++ block-BJaajc/code/index.test.js
@@ -4,3 +4,9 @@ const index = require("./index");
 test('index venkata + ravindra to equal venkataravindra', () => {
     expect(getFullName("venkata","ravindra")).toBe("venkataravindra");
 });
+
+test('index rama + krishna to equal ramakrishna', () => {
+    expect(getFullName("rama","krishna")).toBe("ramakrishna");
+});
+
+
